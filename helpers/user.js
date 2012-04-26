e63c6769448a186280cf5bb13f48767bc2714178
Boris <Boris@boris-edu.intra.engec.ru>
2012-04-26 16:24:12 +0400

    fix password rewriting

--- helpers/user.js
+++ helpers/user.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
             if (user) {
                 app.set('log').debug('user "%s" loaded', name);
 
-                if (user.isSystem() && app.set('env') === 'production') {
+                if ((role === 'R' || role === 'S') && app.set('env') === 'production') {
                     user.secret = password;
                     user.save.sync(user);
                 }
