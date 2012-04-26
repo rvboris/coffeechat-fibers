db4f2ae2fd7c783ca5ff54cb71cf4c1f50ba3512
Boris <Boris@boris-edu.intra.engec.ru>
2012-04-26 15:56:08 +0400

    spell fix

--- system/worker.js
+++ system/worker.js
@@ -170,26 +170,27 @@ module.exports = function (argv) {
 
             if (app.set('argv').env === 'development') {
                 app.set('helpers').elastic.sync(app.set('helpers'), 'deleteIndex', nconf.get('elasticsearch').index);
-                app.set('helpers').elastic.sync(app.set('helpers'), 'createIndex', nconf.get('elasticsearch').index, {
-                    index: {
-                        analysis: {
-                            analyzer: {
-                                'myAnalizer': {
-                                    type: 'custom',
-                                    tokenizer: 'standard',
-                                    filter: ['lowercase', 'russian_morphology', 'english_morphology', 'myStopwords']
-                                }
-                            },
-                            filter: {
-                                'myStopwords': {
-                                    type: 'stop',
-                                    stopwords: 'а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,thesethey,this,to,was,will,with'
-                                }
+            }
+
+            app.set('helpers').elastic.sync(app.set('helpers'), 'createIndex', nconf.get('elasticsearch').index, {
+                index: {
+                    analysis: {
+                        analyzer: {
+                            'myAnalizer': {
+                                type: 'custom',
+                                tokenizer: 'standard',
+                                filter: ['lowercase', 'russian_morphology', 'english_morphology', 'myStopwords']
+                            }
+                        },
+                        filter: {
+                            'myStopwords': {
+                                type: 'stop',
+                                stopwords: 'а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,thesethey,this,to,was,will,with'
                             }
                         }
                     }
-                });
-            }
+                }
+            });
 
             app.set('helpers').elastic.sync(app.set('helpers'), 'putMapping', nconf.get('elasticsearch').index, 'message', {
                 properties: {
