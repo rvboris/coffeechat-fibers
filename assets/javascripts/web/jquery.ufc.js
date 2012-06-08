349b5ebbf70fa4a233595bb963df0f64d5e065d0
Boris <Boris@boris-edu.intra.engec.ru>
2012-06-08 17:03:45 +0400

    allways call callback!!

--- assets/javascripts/web/jquery.ufc.js
+++ assets/javascripts/web/jquery.ufc.js
@@ -1404,16 +1404,11 @@
                     $(window).qtip('api').hide();
                     return;
                 }
-                if (privateMethods.channel.qtips.passwordCallbackParams[2] === null) {
+                if (!privateMethods.channel.qtips.passwordCallbackParams[2]) {
                     privateMethods.channel.qtips.passwordCallbackParams[2] = {};
                 }
                 privateMethods.channel.qtips.passwordCallbackParams[2].password = result;
-                $.fn.sys().channel.subscribe(
-                    privateMethods.channel.qtips.passwordCallbackParams[0],
-                    privateMethods.channel.qtips.passwordCallbackParams[1],
-                    privateMethods.channel.qtips.passwordCallbackParams[2],
-                    privateMethods.channel.qtips.passwordCallbackParams[3]);
-                //$.fn.sys().channel.subscribe.apply(null, privateMethods.channel.qtips.passwordCallbackParams);
+                $.fn.sys().channel.subscribe.apply(null, privateMethods.channel.qtips.passwordCallbackParams);
             },
             'passwordCallbackParams': null
         },
